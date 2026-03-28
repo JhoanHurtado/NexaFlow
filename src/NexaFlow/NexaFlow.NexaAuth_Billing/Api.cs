69bab7b81c02cca289fe0eea03f40585d790de37
Jhoan Hurtado <jhoanezequielh@gmail.com>
2026-03-28 12:06:50 -0500

    Add CORS support and update deployment configurations across multiple services

--- src/NexaFlow/NexaFlow.NexaAuth_Billing/Api.cs
+++ src/NexaFlow/NexaFlow.NexaAuth_Billing/Api.cs
@@ -4,28 +4,19 @@ namespace NexaFlow.NexaAuth_Billing;
 
 public static class Api
 {
-    private const string Origin  = "*";
-    private const string Headers = "Content-Type,Authorization,x-tenant-id,stripe-signature";
-    private const string Methods = "GET,POST,PUT,DELETE,OPTIONS";
+    private static readonly string Origin  = Environment.GetEnvironmentVariable("CORS_ORIGIN") ?? "*";
+    private const string AllowHeaders = "Content-Type,Authorization,x-tenant-id,x-role,stripe-signature";
+    private const string AllowMethods = "GET,POST,PUT,DELETE,OPTIONS";
 
-    public static IHttpResult Ok(object body) =>
-        AddCors(HttpResults.Ok(body));
-
-    public static IHttpResult Created(string location, object body) =>
-        AddCors(HttpResults.Created(location, body));
-
-    public static IHttpResult BadRequest(object body) =>
-        AddCors(HttpResults.BadRequest(body));
-
-    public static IHttpResult NotFound() =>
-        AddCors(HttpResults.NotFound());
-
-    public static IHttpResult InternalServerError(object body) =>
-        AddCors(HttpResults.InternalServerError(body));
+    public static IHttpResult Ok(object body) => AddCors(HttpResults.Ok(body));
+    public static IHttpResult Created(string location, object body) => AddCors(HttpResults.Created(location, body));
+    public static IHttpResult BadRequest(object body) => AddCors(HttpResults.BadRequest(body));
+    public static IHttpResult NotFound() => AddCors(HttpResults.NotFound());
+    public static IHttpResult InternalServerError(object body) => AddCors(HttpResults.InternalServerError(body));
 
     private static IHttpResult AddCors(IHttpResult result) =>
         result
             .AddHeader("Access-Control-Allow-Origin",  Origin)
-            .AddHeader("Access-Control-Allow-Headers", Headers)
-            .AddHeader("Access-Control-Allow-Methods", Methods);
+            .AddHeader("Access-Control-Allow-Headers", AllowHeaders)
+            .AddHeader("Access-Control-Allow-Methods", AllowMethods);
 }
